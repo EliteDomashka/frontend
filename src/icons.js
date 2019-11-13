bdf611b99cd33f222f389dfda18e5fe8b8dff843
Alexey <oleksih@gmail.com>
2019-11-13 17:21:43 +0200

    add missing icons

--- src/icons.js
+++ src/icons.js
@@ -3,9 +3,9 @@ import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 // internal icons
 import {
-  faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+  faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle, faArrowUp, faAngleRight,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle);
+library.add(faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle, faArrowUp, faAngleRight);
 Vue.component('vue-fontawesome', FontAwesomeIcon);
